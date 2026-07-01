8914eb9473dcad91a0fbc6011af5afdf98e49799
LikithaC17 <likithachannegowda@gmail.com>
2026-07-01 23:12:29 +0530

    Time: 0 ms (100%), Space: 13.2 MB (53.28%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        lookup = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in lookup:
+                return [lookup[complement], i]
+
+            lookup[nums[i]] = i
+        
