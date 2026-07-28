34bcae82a177390a3b3e4410898b2130bec1ce9c
LikithaC17 <likithachannegowda@gmail.com>
2026-07-28 13:08:02 +0530

    Time: 3 ms (28.63%), Space: 12.4 MB (88.66%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        count = {}
+
+        for num in arr:
+            count[num] = count.get(num, 0) + 1
+
+        return len(count.values()) == len(set(count.values()))
