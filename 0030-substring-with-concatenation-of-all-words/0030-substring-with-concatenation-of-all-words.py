a7c49611aeefa0e8ad91a70fe1c624812909eb76
LikithaC17 <likithachannegowda@gmail.com>
2026-08-25 09:51:02 +0530

    Time: 22 ms (79.41%), Space: 20.1 MB (14.37%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,44 @@
+from collections import Counter
+
+class Solution(object):
+    def findSubstring(self, s, words):
+        if not s or not words:
+            return []
+
+        word_len = len(words[0])
+        word_count = len(words)
+        total_len = word_len * word_count
+
+        target = Counter(words)
+        res = []
+
+        for offset in range(word_len):
+            left = offset
+            window = Counter()
+            count = 0
+
+            for right in range(offset, len(s) - word_len + 1, word_len):
+                word = s[right:right + word_len]
+
+                if word in target:
+                    window[word] += 1
+                    count += 1
+
+                    while window[word] > target[word]:
+                        left_word = s[left:left + word_len]
+                        window[left_word] -= 1
+                        left += word_len
+                        count -= 1
+
+                    if count == word_count:
+                        res.append(left)
+                        left_word = s[left:left + word_len]
+                        window[left_word] -= 1
+                        left += word_len
+                        count -= 1
+                else:
+                    window.clear()
+                    count = 0
+                    left = right + word_len
+
+        return res
