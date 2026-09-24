690d13caa4f7ef4aeca6d39abe6fc625bff1abf0
LikithaC17 <likithachannegowda@gmail.com>
2026-09-24 16:40:22 +0530

    Time: 1 ms (28.19%), Space: 19.3 MB (87.62%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,19 +1,12 @@
-class Solution(object):
-    def myPow(self, x, n):
-        if n == 0:
-            return 1.0
-
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
         if n < 0:
             x = 1 / x
             n = -n
-
-        result = 1.0
-
-        while n > 0:
-            if n % 2 == 1:
-                result *= x
-
+        ans = 1.0
+        while n:
+            if n % 2:
+                ans *= x
             x *= x
             n //= 2
-
-        return result
+        return ans
